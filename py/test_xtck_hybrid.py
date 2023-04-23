13ff31ac9295f51578643f2b0e5d9406a58b9109
chui-E29 <chenggong.hui@mpinat.mpg.de>
2023-04-23 20:54:22 +0200

    add new function get_occupancy_bootstrap_frame. This function use the scipy.stats.bootstrap.

--- py/test_xtck_hybrid.py
+++ py/test_xtck_hybrid.py
@@ -1,9 +1,67 @@
 import unittest
+from xtck_hybrid import *
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_sim_condition_dict(self):
+        xtck = xtck_hybrid("01-NaK2K-charge/0/k_hybrid.out")
+        self.assertEqual(xtck.sim_condition_dict["wat_number"], 11134)
+        self.assertEqual(xtck.sim_condition_dict["K_number"], 160)
+        self.assertEqual(xtck.sim_condition_dict["frame_number"], 2001)
+        self.assertEqual(xtck.state_df.iloc[0]["S0"], "W")
+        self.assertEqual(xtck.state_df.iloc[0]["S1"], "K")
+        self.assertEqual(xtck.state_df.iloc[0]["S2"], "0")
+        self.assertEqual(xtck.state_df.iloc[0]["S3"], "K")
+        self.assertEqual(xtck.state_df.iloc[0]["S4"], "K")
+        self.assertEqual(xtck.state_df.iloc[0]["S5"], "W")
+        df = xtck.get_state_df()
+        self.assertEqual(df.iloc[1]["S0"], "W")
+        self.assertEqual(df.iloc[1]["S1"], "K")
+        self.assertEqual(df.iloc[1]["S2"], "K")
+        self.assertEqual(df.iloc[1]["S3"], "0")
+        self.assertEqual(df.iloc[1]["S4"], "K")
+        self.assertEqual(df.iloc[1]["S5"], "W")
+    def test_get_occupancy(self):
+        xtck = xtck_hybrid("01-NaK2K-charge/0/k_test.out")
+        self.assertEqual(xtck.sim_condition_dict["frame_number"], 10)
+        occ_dict = xtck.get_occupancy()
+        self.assertAlmostEqual(occ_dict["S0_K"], 0.0)
+        self.assertAlmostEqual(occ_dict["S0_0"], 0.1)
+        self.assertAlmostEqual(occ_dict["S0_W"], 0.9)
+        self.assertAlmostEqual(occ_dict["S1_K"], 1)
+
+        self.assertAlmostEqual(occ_dict["S2_K"], 0.4)
+        self.assertAlmostEqual(occ_dict["S2_0"], 0.6)
+
+        self.assertAlmostEqual(occ_dict["S4_K"], 0.7)
+        self.assertAlmostEqual(occ_dict["S4_W"], 0.1)
+        self.assertAlmostEqual(occ_dict["S4_0"], 0.2)
+
+        self.assertAlmostEqual(occ_dict["S5_K"], 0.0)
+        self.assertAlmostEqual(occ_dict["S5_W"], 1)
+        self.assertAlmostEqual(occ_dict["S5_0"], 0.0)
+
+    def test_get_occu_numpy(self):
+        xtck = xtck_hybrid("01-NaK2K-charge/0/k_test.out")
+        occ = xtck.get_occu_numpy(xtck.state_df["S0"],
+                                  xtck.state_df["S1"],
+                                  xtck.state_df["S2"],
+                                  xtck.state_df["S3"],
+                                  xtck.state_df["S4"],
+                                  xtck.state_df["S5"],)
+        keys = []
+
+        occ_dict = xtck.get_occupancy()
+        self.assertListEqual(occ, [occ_dict[k] for k in xtck.occ_keys])
+
+    def test_get_occupancy_bootstrap_frame(self):
+        xtck = xtck_hybrid("01-NaK2K-charge/0/k_hybrid.out")
+        res, confidence_dict = xtck.get_occupancy_bootstrap_frame()
+        print(res.confidence_interval)
+
+
+
+
 
 
 if __name__ == '__main__':
--- py/xtck_hybrid.py
+++ py/xtck_hybrid.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 import pandas as pd
+import numpy as np
+from scipy import stats
+from scipy.stats import bootstrap
+
 
 # read xtck output
 
@@ -9,19 +13,102 @@ class xtck_hybrid:
         if not self.hybrid_out.is_file():
             raise IOError(str(hybrid_out) + " not exist.")
         with open(self.hybrid_out) as f:
-            lines = f.readable()
+            lines = f.readlines()
         if len(lines) <= 24:
             raise ValueError("Please check the xtck output " + str(hybrid_out) + ", less than 24 lines found.")
 
         # load SF states:
         self.sim_condition_dict = {}
         read_flag = False
-        state_count_dict = {}
+        state_count_dict = {
+            "Time": [],
+            "S0": [],
+            "S1": [],
+            "S2": [],
+            "S3": [],
+            "S4": [],
+            "S5": []}
         frame_count = 0
         for l in lines:
             if not read_flag:
                 if "Nr. of waters in input" in l:
-                    self.sim_condition_dict["K_number"] = int(l.split()[-1])
+                    self.sim_condition_dict["wat_number"] = int(l.split()[-1])
                     read_flag = True
+                elif "Nr. of K ions in input" in l:
+                    self.sim_condition_dict["K_number"] = int(l.split()[-1])
+
+
             else:
-                pass # load SF states in each frame
+                if "Nr. of K+ permeation up" in l:
+                    read_flag = False
+                    break
+                words = l.split()
+                state_count_dict["Time"].append(float(words[0]))
+                state = words[4]
+                state_count_dict["S0"].append(state[0])
+                state_count_dict["S1"].append(state[1])
+                state_count_dict["S2"].append(state[2])
+                state_count_dict["S3"].append(state[3])
+                state_count_dict["S4"].append(state[4])
+                state_count_dict["S5"].append(state[5])
+                frame_count += 1
+        self.sim_condition_dict["frame_number"] = frame_count
+        self.state_df = pd.DataFrame(state_count_dict)
+
+        self.occ_keys = []
+        for site in ["S0", "S1", "S2", "S3", "S4", "S5"]:
+            for element in ["K", "0", "W"]:
+                self.occ_keys.append(site + "_" + element)
+
+    def get_state_df(self):
+        return self.state_df
+
+    def get_occu(self, df):
+        occup_dict = {}
+        for site in ["S0", "S1", "S2", "S3", "S4", "S5"]:
+            for element in ["K", "0", "W"]:
+                occup_dict[site + "_" + element] = np.mean(df[site] == element)
+        return occup_dict
+
+    def get_occupancy(self):
+        return self.get_occu(self.state_df)
+
+    def get_occu_numpy(self, d0, d1, d2, d3, d4, d5, ):
+        occ = []
+        for k, data in zip(self.occ_keys, [d0, d0, d0,
+                                           d1, d1, d1,
+                                           d2, d2, d2,
+                                           d3, d3, d3,
+                                           d4, d4, d4,
+                                           d5, d5, d5, ]):
+            occ.append(np.mean(data == k[-1]))
+        return occ
+
+    def get_occupancy_bootstrap_frame(self, n_resamples=1000, confidence=0.95, method='BCa'):
+        """
+        :param n_resamples: The number of resamples performed to form the bootstrap distribution of the statistic.
+        :param confidence: The confidence level of the confidence interval.
+        :return:
+            BootstrapResult :An object with attributes: confidence_interval, bootstrap_distribution, standard_error
+            confidence_dict :A dictionary which contain the lower and higher boundary of the confidence interval
+        """
+        data = (self.state_df["S0"],
+                self.state_df["S1"],
+                self.state_df["S2"],
+                self.state_df["S3"],
+                self.state_df["S4"],
+                self.state_df["S5"],)
+        res = bootstrap(data,
+                        self.get_occu_numpy,
+                        n_resamples=n_resamples,
+                        paired=True,
+                        confidence_level=confidence, vectorized=False, axis=0, method=method )
+        confidence_dict = {}
+        for i in range(len(self.occ_keys)):
+            k = self.occ_keys[i]
+            confidence_dict[k] = [res.confidence_interval.low[i],
+                                  res.confidence_interval.high[i],
+                                  ]
+        return res, confidence_dict
+
+
